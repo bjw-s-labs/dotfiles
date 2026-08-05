5cf8218f09c2ecd9440341e369026def6cfd02ce
Bernd Schorgers <me@bjw-s.dev>
2026-08-05 07:52:52 +0200

    feat: Update fish and ghostty config

--- home/dot_config/fish/conf.d/00-env.fish
+++ home/dot_config/fish/conf.d/00-env.fish
@@ -7,4 +7,6 @@ set -gx VISUAL vim
 set -gx EDITOR vim
 set -gx KUBE_EDITOR vim
 
+set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
 fish_add_path --global --prepend $HOME/.local/bin
---
+++ home/dot_config/fish/conf.d/kanidm.fish
@@ -0,0 +1,3 @@
+if type -q kanidm
+    set -gx KANIDM_URL https://idm.bjw-s.dev/ui/login
+end
